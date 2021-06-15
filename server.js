0086b3ed452b821177c8ced9b5599aad6ddc69bd
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-06-15 18:52:14 +0800

    updating database

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const image = require('./controllers/image');
 const database = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
     }
 });
