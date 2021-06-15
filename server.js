83d629edeca9f7e35de6a58f2a57eae0a247d399
Jean <81425194+jenchen-dev@users.noreply.github.com>
2021-06-15 13:50:47 +0800

    changeing response

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users) })
+app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin', signin.handleSignIn(database, bcrypt));
 app.post('/register', register.handleRegister(database, bcrypt));
 app.get('/profile/:id', profile.handleProfileGet(database));
